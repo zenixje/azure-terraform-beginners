c2a881c5cc6da2fc450824e06d8710bb577597ca
Eric Laar van <33876752+ericvanlaargmailcom@users.noreply.github.com>
2020-06-11 14:12:09 +0200

    dogapp

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "prefix" {
 
 variable "hostname" {
   description = "Virtual machine hostname. Used for local hostname, DNS, and storage-related names."
-  default     = "catapp"
+  default     = "dogapp"
 }
 
 variable "location" {
